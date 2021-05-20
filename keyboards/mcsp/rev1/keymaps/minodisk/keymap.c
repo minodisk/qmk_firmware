c2c3fd1f280c71465bb148148562d2ed6d9a5ab5
minodisk <daisuke.mino@gmail.com>
2021-05-20 11:59:51 +0900

    update

--- keyboards/mcsp/rev1/keymaps/minodisk/keymap.c
+++ keyboards/mcsp/rev1/keymaps/minodisk/keymap.c
@@ -19,24 +19,24 @@ enum layers{
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
     /* Left */                                                  /* Right */
-    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,
+     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS,
     KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,
       G_SPC,     KAO, KC_LALT, KC_LGUI,  KC_ESC,  RAISE0,        RAISE1, KC_BSPC,  KC_ENT, KC_LALT,    CAPT,   A_SPC
   ),
   [_RAISE0] = LAYOUT(
     /* Left */                                                  /* Right */
-    KC_TILD,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PPLS,
+    KC_TILD,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_PLUS,
     _______, G(KC_A), G(KC_S), G(KC_D), G(KC_F), G(KC_G),       KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_SCLN, _______,
     _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), G(KC_B),       G(KC_N), G(KC_M), KC_LBRC, KC_RBRC, KC_SLSH, _______,
-    _______, _______, _______, _______, _______,   G_SPC,         G_SPC, _______, _______, _______, _______, _______
+    KC_WAKE, _______, _______, _______, _______, XXXXXXX,         G_SPC, _______, _______, _______, _______, _______
   ), 
   [_RAISE1] = LAYOUT(
     /* Left */                                                  /* Right */
  A(KC_TILD), KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_EQL,
-    _______, G(KC_A), G(KC_S), G(KC_D), G(KC_F), G(KC_G),       KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,S(KC_SCLN),S(KC_QUOT),
-    _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), G(KC_B),       G(KC_N), G(KC_M),S(KC_LBRC),S(KC_RBRC),S(KC_SLSH),S(KC_BSLS),
-    _______, _______, _______, _______, _______,   A_SPC,         A_SPC, _______, _______, _______, _______, _______
+    _______, G(KC_A), G(KC_S), G(KC_D), G(KC_F), G(KC_G),       KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_COLN,  KC_DQT,
+    _______, G(KC_Z), G(KC_X), G(KC_C), G(KC_V), G(KC_B),       G(KC_N), G(KC_M), KC_LCBR, KC_RCBR, KC_QUES, KC_PIPE,
+    EEP_RST,   RESET, _______, _______, _______,   A_SPC,       XXXXXXX, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, KC_SLEP
   ), 
 };
 
