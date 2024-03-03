6d397283812be4af5acd0dee5ee355b0bba59e38
mino <daisuke.mino@gmail.com>
2024-03-04 00:06:18 +0900

    add keymap minodisk

--- keyboards/sporewoh/minipeg48/keymaps/minodisk/keymap.c
+++ keyboards/sporewoh/minipeg48/keymaps/minodisk/keymap.c
@@ -30,133 +30,87 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 
-#define L2_SPC   LT(_LOWER,KC_SPC)
-#define L3_ESC   LT(_RAISE,KC_ESC)
-#define GUI_SPC  GUI_T(KC_SPC)
-#define GUI_EN   GUI_T(KC_LNG2)
-#define ALT_EN   ALT_T(KC_LNG2)
-#define CTL_JA   CTL_T(KC_LNG1)
-#define ALT_JA   ALT_T(KC_LNG1)
-#define SC_SLEP  G(A(KC_PWR))
+#define L2_ESC   LT(_LOWER,KC_ESC)
+#define L3_SPC   LT(_RAISE,KC_SPC)
+#define SC_SLEEP LAG(KC_PWR)
 #define SC_IME   G(KC_SPC)
 #define SC_SPOT  A(KC_SPC)
 #define SC_SSF   G(S(KC_4))
 #define SC_SSC   G(C(S(KC_4)))
 #define SC_EMO   G(C(KC_SPC))
 
-#define SC_1     G(C(S(A(KC_1))))
-#define SC_2     G(C(S(A(KC_2))))
-#define SC_3     G(C(S(A(KC_3))))
-#define SC_4     G(C(S(A(KC_4))))
-#define SC_5     G(C(S(A(KC_5))))
-#define SC_6     G(C(S(A(KC_6))))
-#define SC_7     G(C(S(A(KC_7))))
-#define SC_8     G(C(S(A(KC_8))))
-#define SC_9     G(C(S(A(KC_9))))
-#define SC_0     G(C(S(A(KC_0))))
-
-#define SC_A     G(C(S(A(KC_A))))
-#define SC_B     G(C(S(A(KC_B))))
-#define SC_C     G(C(S(A(KC_C))))
-#define SC_D     G(C(S(A(KC_D))))
-#define SC_E     G(C(S(A(KC_E))))
-#define SC_F     G(C(S(A(KC_F))))
-#define SC_G     G(C(S(A(KC_G))))
-#define SC_H     G(C(S(A(KC_H))))
-#define SC_I     G(C(S(A(KC_I))))
-#define SC_J     G(C(S(A(KC_J))))
-
-#define SC_K     G(C(S(A(KC_K))))
-#define SC_L     G(C(S(A(KC_L))))
-#define SC_M     G(C(S(A(KC_M))))
-#define SC_N     G(C(S(A(KC_N))))
-#define SC_O     G(C(S(A(KC_O))))
-#define SC_P     G(C(S(A(KC_P))))
-#define SC_Q     G(C(S(A(KC_Q))))
-#define SC_R     G(C(S(A(KC_R))))
-#define SC_S     G(C(S(A(KC_S))))
-#define SC_T     G(C(S(A(KC_T))))
-
-#define SC_U     G(C(S(A(KC_U))))
-#define SC_V     G(C(S(A(KC_V))))
-#define SC_W     G(C(S(A(KC_W))))
-#define SC_X     G(C(S(A(KC_X))))
-#define SC_Y     G(C(S(A(KC_Y))))
-#define SC_Z     G(C(S(A(KC_Z))))
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |  -   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Screen| Ctrl | Shift|  Alt |  Cmd |  Spc |  Esc | BSPC | Enter|  En  |  Ja  |  Emo |
- * `-----------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------.
+ * | Tab |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  -  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * | Ctl |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |Shift|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |  \  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * | Ja  | Scr | Alt | Bs  | Cmd | Esc |Space| Ctl |Enter|Shift| Emo | En  |
+ * `-----------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    SC_SSC,  KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, L2_SPC,  L3_ESC,  KC_BSPC, KC_ENT,  KC_LNG2, KC_LNG1, SC_EMO
+    KC_LNG1, SC_SSF,  KC_LALT, KC_BSPC, KC_LGUI, L2_ESC,  L3_SPC,  KC_RCTL, KC_ENT,  KC_RSFT, SC_EMO,  KC_LNG2
 ),
 
 /* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | End  |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------.
+ * |  `  |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  =  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |  ←  |  ↑  |  ↓  |  →  |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |  [  |  ]  |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |HYPR1|HYPR2|HYPR3|HYPR4|HYPR5|     |     |HYPR6|HYPR7|HYPR8|HYPR9|HYPR0|
+ * `-----------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
-    _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
-    _______, _______, _______, _______, _______, _______,   _______, _______, KC_LBRC, KC_RBRC, _______, _______,
-    SC_1,    SC_2,    SC_3,    SC_4,    SC_5,    XXXXXXX,   MO(4),   SC_6,    SC_7,    SC_8,    SC_9,    SC_0
+    KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,       KC_0,       KC_EQL,
+    _______,    _______,    _______,    _______,    _______,    _______, KC_LEFT, KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
+    _______,    _______,    _______,    _______,    _______,    _______, _______, _______,    KC_LBRC,    KC_RBRC,    _______,    _______,
+    HYPR(KC_1), HYPR(KC_2), HYPR(KC_3), HYPR(KC_4), HYPR(KC_5), XXXXXXX, MO(4),   HYPR(KC_6), HYPR(KC_7), HYPR(KC_8), HYPR(KC_9), HYPR(KC_0)
 ),
 
 /* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------.
+ * |  ~  |  !  |  @  |  #  |  $  |  %  |  ^  |  &  |  *  |  (  |  )  |  +  |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |  ←  |  ↓  |  ↑  |  →  |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |  {  |  }  |     |     |
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-    _______, _______, _______, _______, _______, _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
-    _______, _______, _______, _______, _______, XXXXXXX,   SC_SPOT, _______, KC_LCBR, KC_RCBR, _______, _______,
-    SC_A,    SC_B,    SC_C,    SC_D,    SC_E,    MO(4),     XXXXXXX, SC_F,    SC_G,    SC_H,    SC_I,    SC_J
+    KC_TILD,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_PLUS,
+    _______,    _______,    _______,    _______,    _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LCBR,    KC_RCBR,    _______,    _______,
+    HYPR(KC_A), HYPR(KC_B), HYPR(KC_C), HYPR(KC_D), HYPR(KC_E), MO(4),      XXXXXXX,    HYPR(KC_F), HYPR(KC_G), HYPR(KC_H), HYPR(KC_I), HYPR(KC_J)
 ),
 
 /* Adjust (Lower + Raise)
- *                      v------------------------RGB CONTROL--------------------v
- * ,-----------------------------------------------------------------------------------.
- * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |      |      |      |      |
- * `-----------------------------------------------------------------------------------'
+ * ,-----------------------------------------------------------------------.
+ * |     | Fn1 | Fn2 | Fn3 | Fn4 | Fn5 | Fn6 | Fn7 | Fn8 | Fn9 | Fn10|Power|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     | Prev| Vol-| Vol+| Next|     |Sleep|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     | Boot|
+ * |-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----|
+ * |     |     |     |     |     |     |     |     |     |     |     |     |
+ * `-----------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_ortho_4x12(
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLU,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_PWR,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,   KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY,   KC_MPLY, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SC_SLEP,
-    SC_K,    SC_L,    SC_M,    SC_N,    SC_O,    XXXXXXX,   XXXXXXX, SC_P,    SC_Q,    SC_R,    SC_S,    SC_T
+    XXXXXXX,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_PWR,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, KC_MPRV, KC_VOLD,    KC_VOLU,    KC_MNXT,    XXXXXXX,    SC_SLEEP,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, KC_MPLY, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    QK_BOOT,
+    HYPR(KC_K), HYPR(KC_L), HYPR(KC_M), HYPR(KC_N), HYPR(KC_O), XXXXXXX, XXXXXXX, HYPR(KC_P), HYPR(KC_Q), HYPR(KC_R), HYPR(KC_S), HYPR(KC_T)
 )
 
 };
