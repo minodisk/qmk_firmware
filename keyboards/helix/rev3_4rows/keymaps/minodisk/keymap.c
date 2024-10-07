b2301d110d6677f79a39a7ce3e78cecb8528a8af
mino <daisuke.mino@gmail.com>
2024-10-07 09:49:16 +0900

    Update

--- keyboards/helix/rev3_4rows/keymaps/minodisk/keymap.c
+++ keyboards/helix/rev3_4rows/keymaps/minodisk/keymap.c
@@ -71,10 +71,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_MAC] = LAYOUT(
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-      SC_SSC,  SC_SSF,  SC_EMO,  CTL_JA,  ALT_EN,  GUI_SPC,  L2_ESC, L3_SPC,  KC_BSPC, KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_TAB,  KC_Q,   KC_W,   KC_E,            KC_R,   KC_T,                                            KC_Y,    KC_U,    KC_I,            KC_O,    KC_P,    KC_MINS,
+      KC_LCTL, KC_A,   KC_S,   KC_D,            KC_F,   KC_G,                                            KC_H,    KC_J,    KC_K,            KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,   KC_X,   KC_C,            KC_V,   KC_B,                                            KC_N,    KC_M,    KC_COMM,         KC_DOT,  KC_SLSH, KC_BSLS,
+      SC_SSC,  SC_SSF, SC_EMO, LALT_T(KC_LNG1), KC_SPC, KC_LGUI, LT(_LOWER, KC_ENT), LT(_RAISE, KC_ESC), KC_RCTL, KC_BSPC, RSFT_T(KC_LNG2), KC_DOWN, KC_UP,   KC_RGHT
   ),
 
   [_WIN] = LAYOUT(
